73bbef2b7dd9f74826bf133d3ac69b3310910eff
Samuel <samuelldmj5@gmail.com>
2024-08-27 15:45:37 +0100

    modified the activation class file to reject insertion into the db for unsuccessful registration

--- app/Controllers/Activation.php
+++ app/Controllers/Activation.php
@@ -1,5 +1,4 @@
 <?php
-// Handles activation logic
 
 class Activation
 {
@@ -16,18 +15,22 @@ class Activation
             if (!$token) {
                 $data['errors'][] = "Activation code is required.";
             } else {
-                // Find the user by token
-                $userRecord = $user->first(['token' => $token]);
+                // Find the user by token in the session data
+                if ($signupData && $signupData['token'] === $token) {
+                    // Insert the user data into the database
+                    $user->insert($signupData);
 
-                if ($userRecord && $userRecord->email === $signupData['email']) {
-                    // Activate the user
-                    $user->update($userRecord->id, ['active' => 1, 'token' => null]);
+                    // Activate the user by setting active to 1 and clearing the token
+                    $userRecord = $user->first(['email' => $signupData['email']]);
+                    if ($userRecord) {
+                        $user->update($userRecord->id, ['active' => 1, 'token' => null]);
+                    }
 
                     // Clear the signup data from session
                     unset($_SESSION['signup_data']);
 
                     // Redirect to login page with email and password
-                    redirect('login', ['email' => $signupData['email'], 'password' => $signupData['password']]);
+                    redirect('login', ['email' => $signupData['email'], 'password' => $_POST['password']]);
                 } else {
                     $data['errors'][] = "Invalid activation code or email.";
                 }
@@ -35,7 +38,7 @@ class Activation
         }
 
         $data['title'] = 'Activate Account';
-        // Data helps us pass data into the activation page here to be viewed
+        // Pass the data to the activation view
         $this->views('activation', $data);
     }
 }
